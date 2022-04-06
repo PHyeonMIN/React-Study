bfc55064307fae09ecc98432258af74fc857e2b4
PHyeonMIN <dndb4902@naver.com>
2022-04-06 21:59:13 +0900

    2022-04-06

---
+++ first/src/components/Card.js
@@ -0,0 +1,30 @@
+import './Card.css';
+
+function Card(props) {
+    const classes = 'card '+ props.className;
+    return(
+        <div className={classes}>{props.children}</div>
+        // children : 지정된 속성, 오프닝 태그와 클로징 태그 사이의 콘텐츠
+    );
+}
+
+export default Card;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 컴포지션 : 컴포넌트를 합치는 것
+// wrapper 컴포넌트
--- first/src/components/ExpenseItem.js
+++ first/src/components/ExpenseItem.js
@@ -1,18 +1,17 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from './Card';
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67
 
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
