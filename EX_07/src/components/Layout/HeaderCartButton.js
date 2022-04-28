5274a71289ae7aae467fe01149cb791fd370224e
PHyeonMIN <dndb4902@naver.com>
2022-04-28 11:22:25 +0900

    2022-04-28

--- EX_07/src/components/Layout/HeaderCartButton.js
+++ EX_07/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React,{ useContext } from "react";
+import React,{ useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext  from "../../store/cart-context";
@@ -6,14 +6,37 @@ import CartContext  from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
+  const {items} = cartCtx;
+
+
+  const numberOfCartItems = items.reduce((curNumber, item)=>{
       return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump: ''}`;
+
+  useEffect(() => {
+    if(items.length ===0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(()=>{
+      setBtnIsHighlighted(false); 
+    },300);
+    
+    // cleanup함수 : 컴포넌트가 지워져야 할 때 타이머도 지우는 함수
+    return () => {
+      clearTimeout(timer);
+      // 타이머가 만료되기 전에 또 설정시 item을 빠르게 여러개 추가하면 이전 타이머를 지우고 새 타이머가 설정한 뒤, 전 타이머는 지워져야한다.
+    };
+  },[items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- EX_07/src/store/CartProvider.js
+++ EX_07/src/store/CartProvider.js
@@ -2,20 +2,90 @@ import React, { useReducer } from "react";
 
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (itme) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(        // findIndex() : 배열 안의 item index를 찾아준다.
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if(existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }else {
+      updatedItems = state.items.concat(action.item);       // push : 기존의 배열 수정 , concat : 새로운 배열 생성
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  if(action.type==='REMOVE'){
+    const existingCartItemIndex = state.items.findIndex(       
+       (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if(existingItem.amount === 1){
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    }else {
+      const updatedItem = {...existingItem, amount: existingItem.amount-1};
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount : updatedTotalAmount
+    };
+  }
+
+
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
 
   return (
-    <CartContext.Provider vlaue={cartContext}>
+    <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
   );
