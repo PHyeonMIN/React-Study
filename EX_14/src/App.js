911a7acafbdae57527c76c2b6656c65ce30e4909
PHyeonMIN <dndb4902@naver.com>
2022-05-17 19:19:29 +0900

    2022-05-17

--- EX_14/src/App.js
+++ EX_14/src/App.js
@@ -4,9 +4,12 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NoFound";
 
 function App() {
   return (
+    <Layout>
     <Switch>
       <Route path="/" exact>
         <Redirect to="/quotes" />
@@ -14,13 +17,17 @@ function App() {
       <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
-      <Route path="quotes/:quoteId">
+      <Route path="/quotes/:quoteId">
         <QuoteDetail />
       </Route>
       <Route path="/new-quote">
         <NewQuote />
       </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
     </Switch>
+    </Layout>
   );
 }
 
